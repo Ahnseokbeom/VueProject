89c92935a9b171d3768561935e4ccd0982545969
seobkeom <ajm153123@naver.com>
2023-01-14 18:25:11 +0900

    vuetify Insert

--- VueP5/front/src/main.js
+++ VueP5/front/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import {router} from './routes/index.js'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
@@ -9,4 +10,5 @@ Vue.prototype.$http = axios;
 new Vue({
   render: h => h(App),
   router,
+  vuetify,
 }).$mount('#app')
