a05d9a4e97d8cdcd08eca84fdda09b7abedb8806
seobkeom <ajm153123@naver.com>
2023-04-12 21:02:54 +0900

    Backend Update

--- Movie/backend/src/main/java/game/backend/controller/GameController.java
+++ Movie/backend/src/main/java/game/backend/controller/GameController.java
@@ -24,5 +24,46 @@ public class GameController {
 		List<Game> rank = gameRepository.findTop10ByOrderByRanking();
 		return rank;
 	}
+	@GetMapping("api/aos")
+	public List<Game> getAos(){
+		List<Game> aos = gameRepository.findTop10ByType("AOS");
+		return aos;
+	}
+	@GetMapping("api/rpg")
+	public List<Game> getRpg(){
+		List<Game> rpg = gameRepository.findTop10ByType("RPG");
+		return rpg;
+	}
+	@GetMapping("api/fps")
+	public List<Game> getFps(){
+		List<Game> fps = gameRepository.findTop10ByType("FPS");
+		return fps;
+	}
+	@GetMapping("api/as")
+	public List<Game> getActSpt(){
+		List<Game> as = gameRepository.findTop10ByTypeOrType("스포츠","액션");
+		return as;
+	}
+	@GetMapping("api/detailAos")
+	public List<Game> getDetailAos(){
+		List<Game> detail = gameRepository.findByType("AOS");
+		return detail;
+	}
+	@GetMapping("api/detailRpg")
+	public List<Game> getDetailRpg(){
+		List<Game> detail = gameRepository.findByType("RPG");
+		return detail;
+	}
+	@GetMapping("api/detailfps")
+	public List<Game> getDetailFps(){
+		List<Game> detail = gameRepository.findByType("FPS");
+		return detail;
+	}
+	@GetMapping("api/detailAs")
+	public List<Game> getDetailActSpt(){
+		List<Game> detail = gameRepository.findByTypeOrType("스포츠","액션");
+		return detail;
+	}
+	
 
 }
--- Movie/backend/src/main/java/game/backend/repository/GameRepository.java
+++ Movie/backend/src/main/java/game/backend/repository/GameRepository.java
@@ -3,10 +3,13 @@ package game.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import game.backend.entity.Game;
 
 public interface GameRepository extends JpaRepository<Game, Integer>{
 	List<Game> findTop10ByOrderByRanking();
+	List<Game> findTop10ByType(String type);
+	List<Game> findByType(String type);
+	List<Game> findTop10ByTypeOrType(String type1, String type2);
+	List<Game> findByTypeOrType(String type1, String type2);
 }
